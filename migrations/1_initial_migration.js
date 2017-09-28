863aff4c9a27adfefea85978cd9f8575e56f329d
Holy CTO <cto@holytransaction.com>
2017-09-28 15:09:59 +0000

    Also deploy migrations using the first address

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+
+  const ownerAgent = '0x1C3A73709527d0B238467345cB6D0dd03a653215'
+  deployer.deploy(Migrations, {from: ownerAgent});
 };

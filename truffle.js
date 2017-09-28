423e683154adbc3db1020da7327e86daec036de2
Holy CTO <cto@holytransaction.com>
2017-09-28 15:13:14 +0000

    Adding from on truffle.js

--- truffle.js
+++ truffle.js
@@ -4,7 +4,8 @@ module.exports = {
       host: "localhost",
       port: 8545,
       gas:4700000,    //gas for deploy
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: '0x1C3A73709527d0B238467345cB6D0dd03a653215'
     }
   }
 };
